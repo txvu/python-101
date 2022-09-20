37f25c1a2fab9e2dd1fee1bc69a22bb274c83c9b
Trinh Vu <txvu100@gmail.com>
2022-09-20 16:48:20 -0500

    Add Iterators

--- iterators.py
+++ iterators.py
@@ -1,11 +1,20 @@
 def main():
     days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-    list2 = ['hello', 'Isaac', 'a', 'World', 'abc']
+    duties = ['work', 'work', 'work', 'work', 'work', 'play', 'play']
 
     i = iter(days)
     print(next(i))
     print(next(i))
     print(next(i))
+
+    for i, m in enumerate(days, start=1):
+        print(i, m)
+
+    for m in zip(days, duties):
+        print(m)
+
+    for i, m in enumerate(zip(days, duties), start=1):
+        print(i, m[0], 'is for', m[1])
 
 
 if __name__ == '__main__':
