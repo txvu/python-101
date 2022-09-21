dd71a6d41403b247b7aad00f54de577036a75653
Trinh Vu <txvu100@gmail.com>
2022-09-21 10:51:31 -0500

    Added function_doc_strings.py

---
+++ function_doc_strings.py
@@ -0,0 +1,20 @@
+import collections
+
+
+def my_func(arg1, arg2=None):
+    """
+    Doesn't do anything
+    :param arg1: str
+    :param arg2: whatever
+    :return: None
+    """
+
+
+def main():
+    print(map.__doc__)
+    print(collections.__doc__)
+    print(my_func.__doc__)
+
+
+if __name__ == '__main__':
+    main()
